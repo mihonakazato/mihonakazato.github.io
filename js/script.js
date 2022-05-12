e49fe782b801aaa0d6e72d738dfd7162229fbc56
mihonakazato <104816899+mihonakazato@users.noreply.github.com>
2022-05-12 11:16:12 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 'use strict';
+
 $(function() {
+    //フェードイン
     $(window).scroll(function() {
         const windowHeight = $(window).height();
         const scroll = $(window).scrollTop();
 
         $('.fadeIn').each(function() {
             const targetPosition = $(this).offset().top;
-            if(scroll > targetPosition - windowHeight + 100) {
+            if(scroll > targetPosition - windowHeight + 150) {
                 $(this).addClass("scrollIn");
             }
         });
+    });
+
+    //ページ内遷移
+    $('.link a[href*="#"]').click(function() {
+        const hash = $(this).attr('href');
+        const targetPosition = $(hash).offset().top;
+        $('body, html').animate({scrollTop: targetPosition}, 400);
+        return false;
     });
 });
